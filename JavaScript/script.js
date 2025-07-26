e69c8140cb8a8a46cdaae7433a5b06a60d76b15a
Nathaniel Friday <udoakangn@gmail.com>
2025-07-26 16:47:34 +0100

    completed the drop down menu

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -3,7 +3,10 @@ const navToggle = document.querySelector('.navtoggle');
 
 const navItems = document.querySelector('.navitems');
 
+const navSpace = document.querySelector('.navspace')
+
 navToggle.addEventListener('click', () => {
     navToggle.classList.toggle('active');
     navItems.classList.toggle('active');
+    navSpace.classList.toggle('active')
 }); 
