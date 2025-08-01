23442502d4fbb9654e230945faa1b129e2f4e368
Nathaniel Friday <udoakangn@gmail.com>
2025-08-01 09:14:10 +0100

    competed the fourth page for both the 1080px and 880px screen sizes

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -8,5 +8,18 @@ const navSpace = document.querySelector('.navspace')
 navToggle.addEventListener('click', () => {
     navToggle.classList.toggle('active');
     navItems.classList.toggle('active');
-    navSpace.classList.toggle('active')
+    navSpace.classList.toggle('active');
 }); 
+
+// const zoomElement = document.querySelector('.zoomOne');
+// let zoom = 5;
+
+// const ZOOM_SPEED = 0;
+
+// document.addEventListener('wheel', function(e){
+//     if(e.deltaY > 0) {
+//         zoomElement.style.transform = `scale(${(zoom -= ZOOM_SPEED)})`;
+//     }else {
+//         zoomElement.style.transform =`scale(${(zoom += ZOOM_SPEED)})`;
+//     }
+// });
